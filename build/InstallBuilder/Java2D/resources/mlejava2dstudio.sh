16f963d25cde2d5b21888eb26f00ce3afc173a04
Mark Millard <msm@wizzerworks.com>
2022-01-06 15:13:57 -0700

    Update InstallBuilder configurations and resources to remove TCL dependencies.

--- build/InstallBuilder/Java2D/resources/mlejava2dstudio.sh
+++ build/InstallBuilder/Java2D/resources/mlejava2dstudio.sh
@@ -14,31 +14,9 @@ export MLE_WORKPRINTS={{installdir}}
 export LD_LIBRARY_PATH={{installdir}}/lib
 
 #
-# Test for the existence of TCL
-# 
-if [ -z ${TCL_HOME+x} ]; then
-  echo The TCL_HOME environment variable is not set.
-  echo Magic Lantern requires the Tcl scripting language.
-  echo Please install Tcl and set the TCL_HOME variable to
-  echo reference the installed location.
-  zenity --error --text="The TCL_HOME environment variable is not set.\nMagic Lantern requires the Tcl scripting language.\nPlease install Tcl and set the TCL_HOME variable to\nreference the installed location."
-  exit
-else
-  if [ -e $TCL_HOME/bin/tclsh ]; then
-    echo Using $TCL_HOME
-  else
-      echo It appears that Tcl has not been installed.
-      echo Has the TCL_HOME environment variable
-      echo been set to the Tcl installation directory?
-      zenity --error --text="It appears that Tcl has not been installed.\nHas the TCL_HOME environment variable\nbeen set to the Tcl installation directory?"
-      exit
-  fi
-fi
-
-#
 # Set executable path.
 #
-export PATH=$TCL_HOME/bin:$MLE_ROOT/bin:$PATH
+export PATH=$MLE_ROOT/bin:$PATH
 
 #
 # Fork off a new shell running Magic Lantern.
