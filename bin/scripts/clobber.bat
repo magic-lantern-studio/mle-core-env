37fcebdedbdec4d3f8671b62cf9bf706cabe2d7b
Mark Millard <mark.s.millard@gmail.com>
2024-09-18 15:02:10 -0600

    New convenience script to remove 64-bit build artifacts that are installed during development builds.

---
+++ bin/scripts/clobber.bat
@@ -0,0 +1,70 @@
+ECHO OFF
+
+REM This script may be used to remove the 64-bit artifacts that are installed
+REM in %MLE_ROOT% during development builds.
+
+SET CURDIR=%CD%
+
+IF "%MLE_ROOT%"=="" (
+    ECHO MLE_ROOT is NOT defined
+    EXIT
+) ELSE (
+    ECHO MLE_ROOT is %MLE_ROOT%
+    ECHO Libraries will be deleted from %MLE_ROOT%\lib64
+    ECHO Binaraies will be deleted from %MLE_ROOT%\bin64
+    ECHO(
+)
+
+REM Delete libraries.
+IF EXIST %MLE_ROOT%\lib64 (
+    ECHO Clobbering 64-bit libraries
+    CD %MLE_ROOT%\lib64
+    DIR
+    DEL inventor\*.lib
+    DEL rehearsal\*.lib
+    DEL runtime\cdecl\*.lib
+    DEL runtime\stdcall\*.lib
+    DEL tools\*.lib
+    ECHO(
+)
+
+REM Delete binaries.
+IF EXIST %MLE_ROOT%\bin64 (
+    ECHO Clobber 64-bit binaries
+    CD %MLE_ROOT%\bin64
+    DIR
+    DEL *.exe
+    DEL rehearsal\*.dll
+    ECHO(
+)
+
+REM Delete header files.
+IF EXIST %MLE_ROOT%\include (
+    ECHO Clobber header files
+    CD %MLE_ROOT%\include
+    DIR
+    DEL math\*.h
+    DEL mle\*.h
+    DEL rehearsal\mle\mlPlatformData.h
+    DEL runtime\mle\mlPlatformData.h
+    DEL
+    ECHO(
+)
+
+REM Delete miscellaneous files.
+IF EXIST %MLE_ROOT%\include (
+    ECHO Clobber Digital Workprints
+    CD %MLE_ROOT%\include\workprints
+    DIR
+    DEL *.wpp
+    CD %MLE_ROOT%\include\Parts\actors\workprints
+    DEL *.wpa *.wpd
+    CD %MLE_ROOT%\include\Parts\roles\workprints
+    DEL *.wpd
+    CD %MLE_ROOT%\include\Parts\sets\workprints
+    DEL *.wpf
+    ECHO(
+)
+
+REM Clean up.
+CD %CURDIR%
