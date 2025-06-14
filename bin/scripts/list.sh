ed608b00b1757410543a24af7a55bc6ff88f5793
Mark Millard <msm@wizzerworks.com>
2025-06-14 10:26:29 -0600

    New script to list branches in each Magic Lantern component.

---
+++ bin/scripts/list.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# This script needs to be executed from the directory where the git repos
+# have been cloned.
+#
+# Tip: use "ssh-add ~/.ssh/id_rsa" to add your SSH key to the ssh-agent so
+# you dont have to give your passphrase every time "git branch" in invoked.
+
+echo " ***** Processing mle-core-env *****"
+cd mle-core-env
+git branch -a
+
+echo " ***** Processing mle-core-util *****"
+cd ../mle-core-util
+git branch -a
+
+echo " ***** Processing mle-core-math *****"
+cd ../mle-core-math
+git branch -a
+
+echo " ***** Processing mle-core-dwp *****"
+cd ../mle-core-dwp
+git branch -a
+
+echo " ***** Processing mle-core-dpp *****"
+cd ../mle-core-dpp
+git branch -a
+
+echo " ***** Processing mle-core-mlert *****"
+cd ../mle-core-mlert
+git branch -a
+
+echo " ***** Processing mle-studio *****"
+cd ../mle-studio
+git branch -a
+
+echo " ***** Processing mle-parts *****"
+cd ../mle-parts
+git branch -a
+
+echo " ***** Processing mle-atk *****"
+cd ../mle-atk
+git branch -a
+
+echo " ***** Processing mle-loaders *****"
+cd ../mle-loaders
+git branch -a
+
+echo " ***** Processing mle-players *****"
+cd ../mle-players
+git branch -a
+
+echo " ***** Processing mle-core-env *****"
+cd ../mle-titles
+git branch -a
+
+cd ..
